39818159c951ed9b4bfbbe74b6eabd5a5cb0bc04
Sarah Goslee <sarah.goslee@ars.usda.gov>
2018-09-11 18:50:56 -0400

    Fixed APLE percentage removal.

--- R/VFSAPLE.R
+++ R/VFSAPLE.R
@@ -33,11 +33,11 @@ VFSAPLE <- function(x, soilP, OM, manureP = 25, manureSolids = 25, manureWEP = 5
     postVFS <- APLE(soilP, clay, OM, precip, runoff, erosionPost, manureP = 25, manureSolids = 25, manureWEP = 50, manureIn = 40, fertP = 10, fertIn = 40)
 
 
-    # percent change in erosion P and in total P losses
-    pErosion <- 100 * (preVFS$lossErosion - postVFS$lossErosion) / postVFS$lossErosion
+    # P removal efficiency
+    pErosion <- 100 * (1 - postVFS$lossErosion/preVFS$lossErosion)
     pErosion[is.na(pErosion)] <- 0
 
-    pTotal <- 100 * (preVFS$lossTotal - postVFS$lossTotal) / postVFS$lossTotal
+    pTotal <- 100 * (1 - postVFS$lossTotal/preVFS$lossTotal)
     pTotal[is.na(pTotal)] <- 0
 
     list(preVFS=preVFS, postVFS=postVFS, pErosion=pErosion, pTotal=pTotal)
